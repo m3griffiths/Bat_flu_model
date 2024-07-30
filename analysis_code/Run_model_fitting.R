db0a2393df6ed58c98bbcfd222c9c393b7c1e19b
Megan Griffiths <108796441+m3griffiths@users.noreply.github.com>
2024-07-30 12:09:52 +0100

    Update Run_model_fitting.R

--- analysis_code/Run_model_fitting.R
+++ analysis_code/Run_model_fitting.R
@@ -1,7 +1,7 @@
 #This code collates the serology data and uses this data to run model fitting using JAGS.
 
 #set working directory - modify as appropriate to your system
-setwd("Bat_flu_modelling")
+setwd("Bat_flu_model")
 
 #clear the environment
 rm(list=ls())
